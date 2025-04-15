d6bddd254ddaf99bf99eb417c67ed39923ca3fec
Barion <ametrin.studios@gmail.com>
2025-04-15 13:27:43 +0300

    added SlicingTests.Matrix_Rows

--- test/SlicingTests.cs
+++ test/SlicingTests.cs
@@ -11,4 +11,17 @@ public sealed class SlicingTests
         var sliceOfSlice = slice.Slice(1, 3);
         await Assert.That(sliceOfSlice.AsSpan().SequenceEqual([3, 4, 5])).IsTrue();
     }
+
+    [Test]
+    public async Task Matrix_Rows()
+    {
+        var matrix = Matrix.Of(5, 2, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        await Assert.That(matrix.Rows(..5).Storage).IsEqualTo(matrix.Storage);
+        var slice = matrix.Rows(1..4);
+        await Assert.That(slice.AsSpan().SequenceEqual([2, 3, 4, 5, 6, 7])).IsTrue();
+        var sliceOfSlice = slice.Rows(2..3);
+        await Assert.That(sliceOfSlice.AsSpan().SequenceEqual([6, 7])).IsTrue();
+        await Assert.That(matrix.RowRef(3).AsSpan().SequenceEqual([6, 7])).IsTrue();
+        await Assert.That(matrix.RowSpan(2).SequenceEqual([4, 5])).IsTrue();
+    }
 }
